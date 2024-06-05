dc11412e0aad00b177236beb7c764623ae050e29
rafaelsouzasilva <rafael.souza.silva@hotmail.com.br>
2024-06-05 19:20:39 -0300

    feat: create setup application file

---
+++ src/test/kotlin/br/com/rssdev/tasks/TasksApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.rssdev.tasks
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
